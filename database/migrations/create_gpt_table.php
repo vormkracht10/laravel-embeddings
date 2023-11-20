972265438f5688379cda0d584cc74fdaf11f1bb5
Baspa <Baspa@users.noreply.github.com>
2023-11-20 09:15:47 +0000

    Fix styling

--- database/migrations/create_gpt_table.php
+++ database/migrations/create_gpt_table.php
@@ -1,9 +1,9 @@
 <?php
 
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -17,7 +17,7 @@ return new class extends Migration
         if (Schema::connection($this->connection)->hasTable(config('gpt.database.table'))) {
             return;
         }
-        
+
         Schema::create(config('gpt.database.table'), function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('foreign_id');
@@ -26,7 +26,7 @@ return new class extends Migration
         });
 
         $tableName = config('gpt.database.table');
-        
+
         DB::statement("ALTER TABLE {$tableName} ADD COLUMN embedding vector(1536) NULL;");
     }
 };
